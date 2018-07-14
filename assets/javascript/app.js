ee30f98d117dd9f30a2fc80e639087567e6d68d2
Win Le <nguyenwinle@gmail.com>
2018-07-14 16:07:07 -0700

    add start and pause on gif images using click handlers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,41 @@ $(document).ready(function() {
 
     var topics = ["spiderman", "iron man", "thor", "guardians of the galaxy", "captain america", "black panther", "doctor strange"];
     
+    // This function handles events where one button is clicked
+    $("#add-topic").on("click", function(event) {
+        // event.preventDefault() prevents the form from trying to submit itself.
+        // We're using a form so that the user can hit enter instead of clicking the button if they want
+        event.preventDefault();
 
-function gifInfo() {
+        // This line will grab the text from the input box
+        var topic = $("#topic-input").val().trim();
+        // The topic from the textbox is then added to our array
+        topics.push(topic);
 
-    var gifImage = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=APcYrbFKCKhTApeLNz9YIsO6uPMqSgzL&q=" + gifImage + "&limit=10&offset=0&rating=G&rating=PG&lang=en";
+        // calling renderButtons which handles the processing of our topics array
+        renderButtons();
+
+    });
+
+    $('body').on('click', '.gifImg', function() {
+        var src = $(this).attr("src");
+      if($(this).hasClass('playing')){
+         //stop
+         $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
+         $(this).removeClass('playing');
+      } else {
+        //play
+        $(this).addClass('playing');
+        $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
+      }
+    });
+
+    $(document).on("click", ".gifs", gifInfo);
+      
+    function gifInfo() {
+
+        var gifImage = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=APcYrbFKCKhTApeLNz9YIsO6uPMqSgzL&q=" + gifImage + "&limit=10&offset=0&rating=G&rating=PG&lang=en";
 
         $.ajax({
             url: queryURL,
@@ -14,45 +44,41 @@ function gifInfo() {
         }).then(function(response) {
 
             $(".topic").empty();
-    
+        
             console.log(response);
-            // Creating a div to hold the topic
-        
-        for (var i = 0; i < response.data.length; i++) {
-            var image = response.data[i];
-
-            var topicDiv = $("<div class='topic'>");
-    
-            // Storing the rating data
-            var rate = image.rating;
-        
-            // Creating an element to have the rating displayed
-            var pOne = $("<p>").text("Rating: " + rate);
-        
-            // Displaying the rating
-            topicDiv.append(pOne);
-                
-            // get image
-            var photo = image.images.downsized.url;
-    
-        
-            // Creating an element to hold the image
-            var image = $("<img>").attr("src", photo);
-        
-            // Appending the image
-            topicDiv.append(image);
-        
-            // Putting the entire topic above the previous topic
-            $("#topic-view").append(topicDiv);
             
+            for (var i = 0; i < response.data.length; i++) {
+                var image = response.data[i];
 
-    }
-    
+                var topicDiv = $("<div class='topic'>");
+            
+                // Storing the rating data
+                var rate = image.rating;
+                
+                // Creating an element to have the rating displayed
+                var pOne = $("<p>").text("Rating: " + rate);
+                
+                // Displaying the rating
+                topicDiv.append(pOne);
+                        
+                // get image
+                var photo = image.images.fixed_height_still.url;
+                
+                // Creating an element to hold the image
+                var image = $("<img class = 'gifImg'>").attr("src", photo);
+                
+                // Appending the image
+                topicDiv.append(image);
+                
+                // Putting the entire topic above the previous topic
+                $("#topic-view").append(topicDiv);
+                
+            }
+        
         });
     }
 
 
-      
       function renderButtons() {
 
          $("#topic-view").empty();
@@ -75,24 +101,6 @@ function gifInfo() {
             // for each of the elements, create a button for it
       }
     }
-
-    // This function handles events where one button is clicked
-    $("#add-topic").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
-        event.preventDefault();
-
-        // This line will grab the text from the input box
-        var topic = $("#topic-input").val().trim();
-        // The topic from the textbox is then added to our array
-        topics.push(topic);
-
-        // calling renderButtons which handles the processing of our topics array
-        renderButtons();
-
-      });
-
-      $(document).on("click", ".gifs", gifInfo);
 
       // Calling the renderButtons function to display the intial buttons
       renderButtons();
